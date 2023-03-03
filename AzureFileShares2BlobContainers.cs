ecd8c1e0f3dff9dd87dbf6e3a835e90dd0616986
陳鈞 <jim60105@gmail.com>
2023-03-04 06:28:36 +0800

    抽離Logger

--- AzureFileShares2BlobContainers.cs
+++ AzureFileShares2BlobContainers.cs
@@ -27,7 +27,8 @@ namespace AzureFileShares2BlobContainers;
 
 public class AzureFileShares2BlobContainers
 {
-    private static ILogger _logger;
+    private static ILogger Logger => Helper.Log.Logger;
+
     private readonly string[] _extensions = new string[]
     {
         ".jpg",
@@ -44,49 +45,7 @@ public class AzureFileShares2BlobContainers
         ".description"
     };
 
-    public static ILogger Logger
-    {
-        get
-        {
-            if (null == _logger
-                || _logger.GetType() != typeof(Serilog.Core.Logger))
-            {
-                _logger = MakeLogger();
-            }
-            return _logger;
-        }
-        set => _logger = value;
-    }
-
-    public AzureFileShares2BlobContainers()
-    {
-#if DEBUG
-        Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
-#endif
-
-        Logger.Verbose("Starting up...");
-    }
-
     private string[] GetFileNames(string videoId) => _extensions.Select(p => videoId + p).ToArray();
-
-    public static ILogger MakeLogger()
-    {
-        Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
-
-        var logger = new LoggerConfiguration()
-                        .MinimumLevel.Verbose()
-                        .MinimumLevel.Override("Microsoft", LogEventLevel.Fatal)
-                        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Fatal)
-                        .MinimumLevel.Override("System", LogEventLevel.Fatal)
-                        .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] {Message:lj} <{SourceContext}>{NewLine}{Exception}",
-                                         restrictedToMinimumLevel: LogEventLevel.Verbose)
-                        .WriteTo.Seq(serverUrl: Environment.GetEnvironmentVariable("Seq_ServerUrl"),
-                                     apiKey: Environment.GetEnvironmentVariable("Seq_ApiKey"),
-                                     restrictedToMinimumLevel: LogEventLevel.Verbose)
-                        .Enrich.FromLogContext()
-                        .CreateLogger();
-        return logger;
-    }
 
     /// <summary>
     /// Entrypoint!
